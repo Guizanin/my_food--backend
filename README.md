78740ede7e09cc0894df8766473e16783a11fb6a
Guilherme Zanin <guizanin@MacBook-Pro-de-Guilherme.local>
2024-04-02 22:18:10 -0300

    boilerplate struct and documentation readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # boilerplate_springboot
+Template for fast project's starter. To change the names and be happy
+
+<br />
+
+### Configuration:
+
+* Springboot 17;
+* Database H2 in memory (only to development);
+* JPA;
+* Hibernate;
+* Lombok;
+* MapStruct;
+* DevTools;
+
+<br />
+<br />
+
+#### Observation
+* Unit tests configuration's don't done
---
+++ src/main/java/com/agendame/agendame/AgendameApplication.java
@@ -0,0 +1,14 @@
+package com.agendame.agendame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class AgendameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendameApplication.class, args);
+	}
+
+}
